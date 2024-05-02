1ffb9bfff2da961f80e36af391c32d1b1543a44d
dudulukas7 <eduardo.lukasls.777@gmail.com>
2024-05-02 12:07:37 -0300

    add game

---
+++ c++/jogo.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    
+    char palavra[30], letra[1], secreta[30];
+    int tam, i, chances, acertos;
+    bool acerto;
+    
+    chances = 6;
+    tam = 0;
+    i = 0;
+    acerto = false;
+    acertos = 0;
+    
+    cout << "Digite a palavra secreta: \n";
+    cin >> palavra;
+    system ("clear");
+    
+    while (palavra[i] != '\0'){
+        i++;
+        tam++;
+    }
+    
+    for(i=0; i<tam; i++){
+        secreta[i] = '-';
+    }
+    
+    while ((chances > 0) && (acertos < tam)){
+        cout << "Chances restantes: " << chances << "\n\n";
+        cout << "Palvra secreta: ";
+        for(i=0; i<tam; i++){
+            cout << secreta[i];
+        }
+        cout << "\n\nDigite uma letra: ";
+        cin >> letra[0];
+        for(i=0; i<tam; i++){
+            if (palavra[i] == letra[0]){
+                acerto = true;
+                secreta[i] = palavra[i];
+                acertos++;
+            }
+        }
+        if (!acerto){
+            chances--;
+        }
+        acerto = false;
+        system("clear");
+    }
+    if (acertos==tam){
+        cout << "Voce venceu";
+        
+    }else{
+        cout << "perdeu, burro!"; 
+    }
+    return 0;
+    
+}
